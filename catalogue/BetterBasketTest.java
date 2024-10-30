f59da4f0e3b5bb1a733bb777bcf884b97efc973c
EnergeticBark <seth.humphries@live.com>
2024-10-30 14:55:16 +0000

    Unit test for sorting in BetterBasket.

--- catalogue/BetterBasketTest.java
+++ catalogue/BetterBasketTest.java
@@ -18,4 +18,19 @@ class BetterBasketTest {
         assertEquals(2, betterBasket.size(), "Incorrect size after merge");
         assertEquals(3, betterBasket.getFirst().getQuantity(), "Incorrect quantity after merge");
     }
+
+    @org.junit.jupiter.api.Test
+    void testSort() {
+        Product product1 = new Product("0002", "Example Product 2", 25, 1);
+        Product product2 = new Product("0004", "Example Product 4", 75, 1);
+        Product product3 = new Product("0004", "Example Product 4", 75, 1);
+        Product product4 = new Product("0001", "Example Product 1", 100, 1);
+
+        BetterBasket betterBasket = new BetterBasket();
+        betterBasket.add(product1);
+        betterBasket.add(product2);
+        betterBasket.add(product3);
+        betterBasket.add(product4);
+        assertEquals("0001", betterBasket.getFirst().getProductNum(), "Incorrect position after sort");
+    }
 }
