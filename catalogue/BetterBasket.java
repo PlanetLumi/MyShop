25df60a393278d35419beb9494871c55ed926b88
finnkirby <finnkirby5@gmail.com>
2024-11-18 17:15:57 +0000

    added betterBasket to merge same items, as well as sorting items by product number.
    Additionally added NameToNumber class and search button so customers can search by name rather than the product number.

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -12,8 +12,23 @@ import java.util.Collections;
 public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
-  
 
+  @Override
+  public boolean add( Product pr )
+  {
+    for(Product prInList: this) {
+      if(prInList.getProductNum().equals(pr.getProductNum())) {
+        int quantity = pr.getQuantity() + prInList.getQuantity();
+        prInList.setQuantity(quantity);
+        return (true);
+      }
+    }
+    super.add( pr );     // Call add in ArrayList
+
+    //Collections.sort(this);
+    Collections.sort(this,new SortByNum());
+    return (true);
+  }
   // You need to add code here
   // merge the items for same product,
   // or sort the item based on the product number
--- clients/customer/CustomerController.java
+++ clients/customer/CustomerController.java
@@ -29,6 +29,13 @@ public class CustomerController
     model.doCheck(pn);
   }
 
+  // Check by name
+  public void doCheckByName(String name) {
+    NameToNumber nameToNumber = new NameToNumber();
+    String pn = nameToNumber.getNumberByName(nameToNumber, name);
+    model.doCheck(pn);
+  }
+
   /**
    * Clear interaction from view
    */
